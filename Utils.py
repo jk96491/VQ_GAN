e9c8bbaf6038cb66acf16cb9eaf43e911277a5d6
RaptorJung <jk96491@naver.com>
2021-07-15 19:37:15 +0900

    기타 수정

--- Utils.py
+++ Utils.py
@@ -39,6 +39,43 @@ def CIFARLoadData(batch_size, Train, generate_image):
     return data_loader
 
 
+class TrainImageFolder(datasets.ImageFolder):
+
+    def __getitem__(self, index):
+        filename = self.imgs[index]
+        if filename[0].split('\\')[2] == 'out_dry01':
+            label = 0
+        elif filename[0].split('\\')[2] == 'out_normal01':
+            label = 1
+        else:
+            label = 2
+
+        return super(TrainImageFolder, self).__getitem__(index)[0], label
+
+
+def SkinDataLoad(batch_size, generate_image):
+    if generate_image is True:
+        os.makedirs("images", exist_ok=True)
+
+    traindir = 'skinData\\Train'
+    train_loader = torch.utils.data.DataLoader(
+        TrainImageFolder(traindir,
+                         transforms.Compose([
+                             transforms.RandomResizedCrop(64),
+                             transforms.RandomHorizontalFlip(),
+                             #transforms.Grayscale(num_output_channels=1),
+                             transforms.ToTensor(),
+                             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+                             # normalize,
+                         ])),
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=4,
+        pin_memory=True)
+
+    return  train_loader
+
+
 def conv_3x3(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
 
--- run_vqgan.py
+++ run_vqgan.py
@@ -2,13 +2,14 @@ import torch
 from VQ_GAN.Parser_args import parse_Arg
 from torchvision.utils import save_image
 from Utils import CIFARLoadData
+from Utils import SkinDataLoad
 from Utils import get_device
 from  VQ_GAN.model import vq_gan
 
 
 args = parse_Arg()
 
-batch_size = 256
+batch_size = 64
 num_training_updates = 55000
 
 num_hiddens = 128
@@ -24,35 +25,37 @@ decay = 0.99
 
 learning_rate = 1e-3
 
-train_loader = CIFARLoadData(args.batch_size, True, True)
+train_loader = SkinDataLoad(args.batch_size, True)
 
 device = get_device()
 
-model = vq_gan(args, num_hiddens, num_residual_layers, num_residual_hiddens,
-              num_embeddings, embedding_dim,
-              commitment_cost, decay, device).to(device)
 
-for epoch in range(args.n_epochs):
-    for i, data in enumerate(train_loader):
-        real_images, _ = data
-        current_batch_size = real_images.size(0)
+if __name__ == '__main__':
+    model = vq_gan(args, num_hiddens, num_residual_layers, num_residual_hiddens,
+                  num_embeddings, embedding_dim,
+                  commitment_cost, decay, device).to(device)
 
-        inputs = real_images.clone().to(device)
-        noise = torch.zeros(current_batch_size, args.noise_dim, 1, 1).normal_(0, 1)
+    for epoch in range(args.n_epochs):
+        for i, data in enumerate(train_loader):
+            real_images, _ = data
+            current_batch_size = real_images.size(0)
 
-        real_labels = torch.ones(current_batch_size, 1).detach().to(device)
-        fake_labels = torch.zeros(current_batch_size, 1).detach().to(device)
+            inputs = real_images.clone().to(device)
+            noise = torch.zeros(current_batch_size, args.noise_dim, 1, 1).normal_(0, 1)
 
-        discriminator_loss, generator_image = model.learn_discriminator(inputs, real_labels, fake_labels)
-        generator_loss, perplexity = model.learn_generator(inputs, real_labels)
+            real_labels = torch.ones(current_batch_size, 1).detach().to(device)
+            fake_labels = torch.zeros(current_batch_size, 1).detach().to(device)
 
-        print(
-            "[Epoch %d/%d] [Batch %d/%d] [Discriminator_loss: %f] [Generator_loss: %f] [perplexity : %f]"
-            % (epoch + 1, args.n_epochs, i + 1, len(train_loader), discriminator_loss, generator_loss, perplexity)
-        )
+            discriminator_loss, generator_image = model.learn_discriminator(inputs, real_labels, fake_labels)
+            generator_loss, perplexity = model.learn_generator(inputs, real_labels)
 
-        batches_done = epoch * len(train_loader) + i
-        if batches_done % args.sample_interval == 0:
-            temp = generator_image[0:64]
-            save_image(generator_image[0:64], "images/%d.png" % batches_done, nrow=args.nrow, normalize=True)
+            print(
+                "[Epoch %d/%d] [Batch %d/%d] [Discriminator_loss: %f] [Generator_loss: %f] [perplexity : %f]"
+                % (epoch + 1, args.n_epochs, i + 1, len(train_loader), discriminator_loss, generator_loss, perplexity)
+            )
+
+            batches_done = epoch * len(train_loader) + i
+            if batches_done % len(train_loader) == 0 and epoch % 10 == 0:
+                temp = generator_image[0:64]
+                save_image(generator_image[0:64], "images/%d.png" % batches_done, nrow=args.nrow, normalize=True)
 
